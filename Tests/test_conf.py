c4a2683aaf6fc9addf29f58d312401ab48e77032
koshal <koshal>
2021-10-07 18:25:41 +0530

    updated chrome driver

--- Tests/test_conf.py
+++ Tests/test_conf.py
@@ -1,6 +1,6 @@
 import pytest
 from selenium import webdriver
-
+import os
 from Configuration.constants import CHROME_PATH
 
 
@@ -10,8 +10,30 @@ class BaseTest:
     def set_up(self, request):
         # options = webdriver.ChromeOptions()
         # options.headless = True
-        web_driver = webdriver.Chrome(CHROME_PATH)
-        web_driver.maximize_window()
-        request.cls.driver = web_driver
+        # web_driver = webdriver.Chrome(CHROME_PATH)
+        # web_driver.maximize_window()
+        # request.cls.driver = web_driver
+
+
+        os.environ["webdriver.chrome.driver"] = CHROME_PATH
+
+        prefs = {"profile.default_content_setting_values.notifications": 2,
+                 "download.prompt_for_download": False,
+                 "download.directory_upgrade": True
+                 }
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
+        chrome_options.add_argument('headless')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument("start-maximized")
+        chrome_options.add_argument("disable-infobars")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("window-size=1920x1080")
+        chrome_options.add_experimental_option("prefs", prefs)
+        driver = webdriver.Chrome(executable_path=CHROME_PATH, options=chrome_options)
+
+        driver.maximize_window()
+        driver.implicitly_wait(10)
+        request.cls.driver = driver
     # yield
     # web_driver.close()
